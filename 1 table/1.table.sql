75ef5818158db32f9c0d05f71280940f450f37a8
SuriKandler <surikandler@gmail.com>
2023-04-25 13:39:59 +0100

    1st commit for the trial branch

--- 1 table/1.table.sql
+++ 1 table/1.table.sql
@@ -1,6 +1,10 @@
 use humanbodydb
-GO
-create table dbo.body(id int not null identity primary key, 
-bodypart varchar(100) not null, 
-num int not null
+go
+drop table if exists body
+go
+create table dbo.body(
+	id int not null identity primary key, 
+	creaturetype varchar(25) not null,
+	bodypart varchar(100) not null, 
+	num int not null	
 )
